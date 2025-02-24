18067c70af649d483da27a3358f88e81d12efbde
dxhuii <dxhuii@qq.com>
2025-02-25 02:38:08 +0800

    chore: 删除配置文件

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Online Alarm Clock'
+export const appDescription = 'Set the hour and minute for the online alarm clock. The alarm message will appear, and the preselected sound will be played at the set time.'
